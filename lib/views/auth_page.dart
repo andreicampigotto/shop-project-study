2be3f1a294cd1faa721a331097ab4659a1493435
Andrei Campigotto <andreicampigotto@Andreis-MacBook-Pro.local>
2024-04-02 01:21:00 -0300

    created login page

--- lib/views/auth_page.dart
+++ lib/views/auth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shop/views/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
@@ -12,38 +13,28 @@ class AuthPage extends StatelessWidget {
           Container(
             decoration: BoxDecoration(color: Theme.of(context).primaryColor),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 80,
-                  ),
-                  //transform: Matrix4.rotationZ(-8 * pi / 180),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      color: Colors.amber,
-                      boxShadow: const [
-                        BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black,
-                            offset: Offset(0, 2))
-                      ]),
-                  child: const Text(
-                    'Shop',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: 'Anton',
-                    ),
-                  ),
+          Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.08,
+              ),
+              const Text(
+                'Shop',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontFamily: 'Anton',
                 ),
-                AuthForm(),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.16,
+              ),
+              const SizedBox(
+                width: double.infinity,
+                child: AuthForm(),
+              ),
+            ],
           ),
         ],
       ),
--- lib/views/components/auth_form.dart
+++ lib/views/components/auth_form.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum AuthMode { singup, login }
+enum AuthMode { singUp, login }
 
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
@@ -11,14 +11,43 @@ class AuthForm extends StatefulWidget {
 
 class _AuthFormState extends State<AuthForm> {
   final _passwordController = TextEditingController();
-  final AuthMode _authMode = AuthMode.login;
+  final _formKey = GlobalKey<FormState>();
+
+  bool _isLoading = false;
+  AuthMode _authMode = AuthMode.login;
 
   final Map<String, String> _authData = {
     'email': '',
     'password': '',
   };
 
-  void _submit() {}
+  bool _isLogin() => _authMode == AuthMode.login;
+  bool _isSingUp() => _authMode == AuthMode.singUp;
+
+  void _switchAuthMode() {
+    setState(() {
+      if (_isLogin()) {
+        _authMode = AuthMode.singUp;
+      } else {
+        _authMode = AuthMode.login;
+      }
+    });
+  }
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) {
+      return;
+    }
+    setState(() => _isLoading = true);
+
+    _formKey.currentState?.save();
+
+    if (_isLogin()) {
+    } else {}
+
+    setState(() => _isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,71 +56,89 @@ class _AuthFormState extends State<AuthForm> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       elevation: 8,
       child: Container(
+        margin: const EdgeInsets.only(top: 16),
         padding: const EdgeInsets.only(top: 8, left: 16, right: 16),
-        height: 360,
+        height: deviceSize.height * 0.4,
         width: deviceSize.width * 0.88,
         child: Form(
+            key: _formKey,
             child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'E-mail'),
-              keyboardType: TextInputType.emailAddress,
-              onSaved: (email) => _authData['email'] = email ?? '',
-              validator: (_email) {
-                final email = _email ?? '';
-                if (email.trim().isEmpty ||
-                    (!email.contains('@') && !email.contains('.com'))) {
-                  return 'E-mail is required';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-                decoration: const InputDecoration(labelText: 'Password'),
-                keyboardType: TextInputType.emailAddress,
-                onSaved: (password) => _authData['password'] = password ?? '',
-                obscureText: true,
-                controller: _passwordController,
-                validator: (_password) {
-                  final password = _password ?? '';
-                  if (password.isEmpty || password.length < 5) {
-                    return 'Please enter a valid password';
-                  }
-                  return null;
-                }),
-            if (_authMode == AuthMode.singup)
-              TextFormField(
-                decoration:
-                    const InputDecoration(labelText: 'Confirm password'),
-                keyboardType: TextInputType.emailAddress,
-                obscureText: true,
-                validator: _authMode == AuthMode.login
-                    ? null
-                    : (_password) {
-                        final password = _password ?? '';
-                        if (password != _passwordController.text) {
-                          return "Passwords don't match";
-                        }
-                        return null;
-                      },
-              ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: _submit,
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  onSaved: (email) => _authData['email'] = email ?? '',
+                  validator: (_email) {
+                    final email = _email ?? '';
+                    if (email.trim().isEmpty ||
+                        (!email.contains('@') && !email.contains('.com'))) {
+                      return 'E-mail is required';
+                    }
+                    return null;
+                  },
                 ),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-              ),
-              child: Text(
-                _authMode == AuthMode.login ? 'Login' : 'Sing-up',
-                style: const TextStyle(fontSize: 16),
-              ),
-            ),
-          ],
-        )),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    onSaved: (password) =>
+                        _authData['password'] = password ?? '',
+                    obscureText: true,
+                    controller: _passwordController,
+                    validator: (_password) {
+                      final password = _password ?? '';
+                      if (password.isEmpty || password.length < 5) {
+                        return 'Please enter a valid password';
+                      }
+                      return null;
+                    }),
+                if (_isSingUp())
+                  TextFormField(
+                    decoration:
+                        const InputDecoration(labelText: 'Confirm password'),
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: true,
+                    validator: _isLogin()
+                        ? null
+                        : (_password) {
+                            final password = _password ?? '';
+                            if (password != _passwordController.text) {
+                              return "Passwords don't match";
+                            }
+                            return null;
+                          },
+                  ),
+                const SizedBox(height: 24),
+                if (_isLoading)
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: const CircularProgressIndicator())
+                else
+                  ElevatedButton(
+                    onPressed: _submit,
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 8),
+                    ),
+                    child: Text(
+                      _authMode == AuthMode.login ? 'Login' : 'Sing-up',
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                  ),
+                const Spacer(),
+                TextButton(
+                  onPressed: _switchAuthMode,
+                  child: Text(
+                    _isLogin() ? 'Sing up' : 'Sing in',
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
